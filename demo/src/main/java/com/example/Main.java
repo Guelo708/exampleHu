baaab7a72db7df2dd3edf48f1f90a4ca6f7980ba
Guelo708 <mguelo7@gmail.com>
2025-11-22 15:17:09 -0500

    Simplifique el codigo del main

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,10 +1,8 @@
 package com.example;
 
-import com.example.User;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
         // init User History 
         User user = new User();
         user.login();
